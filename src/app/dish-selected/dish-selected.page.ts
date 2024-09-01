024f59ade27bf39d7a720884687d7a08ac7c7e57
alex3952 <85455212+alex3952@users.noreply.github.com>
2024-09-01 15:25:50 -0500

    update ingredientes

--- src/app/dish-selected/dish-selected.page.ts
+++ src/app/dish-selected/dish-selected.page.ts
@@ -20,18 +20,95 @@ export class DishSelectedPage implements OnInit {
       name: 'Encebollado',
       ingredientes: 'Aceite vegetal, Cebolla morada, Tomate, Comino, Salsa de Ají, Agua, Cilantro, Albacora, Yuca, Pimiento verde, Pimiento rojo, Apio, Cubos de caldo de gallina, Diente de Ajo, Limón',
       img: 'https://i.pinimg.com/736x/3b/31/d4/3b31d452a97bbabb17f34875fd44d7fb.jpg',
-      recipe: "prueba"
+      cantidad_ing: `1 Cucharada Aceite Vegetal 
+
+1 1/2 Taza Cebolla Morada picada en tiras finas 
+
+3 Tomates riñón maduros y picados en cuartos 
+
+1/2 Cucharada Comino Molido 
+
+2 Cucharaditas Ají para seco 
+
+1 1/2 Litro Agua 
+
+4 Cucharadas Cilantro fresco picado finamente 
+
+1 libra de albacora fresca, en lomos 
+
+2 Tazas Yuca cortada en cubos 
+
+2/3 Taza Pimiento Rojo picado en pedazos grandes 
+
+2/3 Taza Pimiento Verde Picado en pedazos grandes 
+
+1/2 Taza Apio picado en pedazos grandes 
+
+2 Cubos Caldo De Verduras Maggi® 
+
+2 Dientes de ajo 
+
+4 Unidades Limón cortado en cuartos `,
+      recipe: `1.Coloca en una olla a hervir el agua, el aceite y añade 3 tomates y media cebolla paiteña picados, añade el apio los trozos de pimiento, 2 cucharadas de cilantro, ajos, comino, sal, pimienta y aji para seco.  
+
+2.En otra olla aparte cocina la yuca picada previamente con bastante agua hasta que este suave. 
+
+3.Por aparte realiza el encurtido de cebolla con el resto de cebolla paiteña picada finamente, lavada, escurrida y sazonada con sal, limon y cilantro restante picado. Deja reposar hasta que se curta. 
+
+4.Añade en el caldo del paso 1 los lomos de albacora, cocínala completamente y retira de la olla una vez que estén bien cocidos, aproximadamente 10 minutos. 
+
+5.Luego de reservada la albacora cocida, retira los vegetales del caldo y colócalos en una licuadora, añade un poco de yuca cocida y un poco de líquido de cocción de la yuca, procesalo hasta lograr un liquido homogeneo. Cierne el líquido procesado sobre el caldo de la olla y añade el agua de cocción de las yucas. Disuelve los cubos de Caldo de Verduras MAGGI®. Llevamos a ebullición durante 5 minutos a fuego medio. Regresamos la albacora a la preparación. 
+
+6.Rectificamos los sabores con sal y pimienta. Coloca un poco de sopa con pedazos de yuca y albacora en el plato y sirve acompañado con un poco del curtido de cebolla que preparaste anteriormente. `
     },
     {
       name: 'Chaulafan',
       ingredientes: 'Arroz, Pollo, Arvejas, Aceite vegetal, Cebolla perla, Pimiento verde, Zanahoria, Cubos de caldo de gallina, Cilantro, Platano maduro',
       img: 'https://i.pinimg.com/236x/3f/a1/ff/3fa1ff58470d0f07b15f32fa2056931a.jpg',
-      recipe: "prueba"
+      cantidad_ing: `3 Tazas Arroz 
+
+2 Tazas Pollo desmechado 
+
+1/2 Taza Arvejas cocidas 
+
+1 Cucharada Aceite de achiote 
+
+1/2 Taza Cebolla Perla cortada en tiras 
+
+1/2 Taza Pimiento Verde cortado en tiras 
+
+1/2 Taza Zanahoria cortada en cubos pequeños 
+
+1 Cubo Caldo De Gallina Maggi® 
+
+2 Cucharadas Cilantro finamente picado 
+
+4 Porciones Plátano Maduro frito `,
+      recipe: `1.Cocina el arroz en una olla con el Caldo de Gallina en Cubo MAGGI® y reserva. Del mismo modo, una vez frío, desmenuza la carne de pollo cocida y reserva. Haz lo mismo con las arvejas. Una vez que estén cocidos, tenlos listos para mezclar todo. 
+
+Prepara el refrito 
+
+2.Añade el aceite de achiote en una olla grande y una vez caliente, agrega la cebolla perla y el pimiento. Cocina durante 4 minutos. Luego, añade el pimiento picado y la zanahoria, y continúa cocinando. Sazona todo con el Caldo de Gallina en cubo MAGGI®. 
+
+Mezcla todo 
+
+3.Agrega el arroz cocido, el pollo desmenuzado y las arvejas cocidas en la olla con el refrito. Mezcla bien para incorporar todos los ingredientes. Deja que se caliente y sirve con rodajas de maduro frito como acompañamiento. 
+
+ `
     },
     {
       name: 'Tortilla de huevo',
       ingredientes: 'Huevo, Leche evaporada, Sobre de caldo de gallina, Queso, Aceite vegetal',
       img: 'https://i.pinimg.com/236x/d9/03/8d/d9038d8bee836a3f7faf2376eec28056.jpg',
+      cantidad_ing: `4 Unidades Huevo 
+
+1 Sobre Caldo Criollita MAGGI® 
+
+62 Mililitros Leche Evaporada LA LECHERA® 
+
+90 gramos Queso Fresco 
+
+15 Mililitros Aceite De Girasol `,
       recipe: `Prepara la mezcla 
 
 1.Rompe los huevos y bátelos junto con CRIOLLITA MAGGI®, la Leche Evaporada LA LECHERA® y el queso fresco. 
@@ -48,6 +125,31 @@ Sirve
       name: 'Estofado de carne',
       ingredientes: 'Cebolla perla, Papa, Diente de Ajo, Carne de Res, Tomillo fresco, Laurel, Pasta de tomate, Vino tinto, Zanahoria, Aceite vegetal, Arvejas, Sal, Pimienta negra',
       img: 'https://i.ytimg.com/vi/39d9fGWD6V4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA5XSo08vwKOORkqctNhG-P60cYHg',
+      cantidad_ing: `1/2 Unidad Cebolla Perla cortada en cubitos 
+
+2 unidades de papa grande picadas en cubos medianos 
+
+4 dientes de ajo picado finamente 
+
+3 Tazas De Carne De Res picada en cubos medianos 
+
+Tomillo Fresco al gusto 
+
+2 Hojas De Laurel 
+
+1 Taza MAGGI® La Rojita Puré de Tomate 
+
+1/2 Copa de Vino Tinto 
+
+2 Unidades Zanahoria picada en cubos 
+
+2 Unidades Caldo De Costilla Maggi® 
+
+3 Cucharadas Aceite Vegetal 
+
+1/2 Taza Arvejas precocidas en agua 
+
+Sal y Pimienta al gusto `,
       recipe: `En el sartén 
 
 1.Coloca el aceite en un sartén amplio, una vez caliente coloca y sella la carne. Retira del sartén y reserva. 
@@ -68,6 +170,37 @@ Sirve
       name: 'Cazuela de pescado',
       ingredientes: 'Platano verde, Mantequilla, Cebolla larga, Sal prieta, Maní tostado, Leche semidescremada, Aceite vegetal, Cebolla morada, Tomate, Pimiento verde, Comino, Sobre de caldo de gallina, Albacora, Cilantro, Limón',
       img: 'https://www.recetasnestle.com.ec/sites/default/files/srh_recipes/cda13d770e8135d603f5d37d1080c00f.jpg',
+      cantidad_ing: `5 Unidades Plátano Verde rallados finamente 
+
+2 Cucharadas Aceite de achiote 
+
+1 Cucharada Mantequilla Con Sal 
+
+1/2 Taza Cebolla Morada picada finamente 
+
+1 Unidad Tomate picado finamente 
+
+1/2 Taza Cebolla Larga 
+
+3/4 Taza Pimiento Verde picado finamente 
+
+1 Cucharadita Comino Molido 
+
+1/2 Sobre Caldo verduras en polvo MAGGI® 
+
+1 Cucharadita Sal Prieta para acompañar 
+
+1/2 Taza Maní tostado ligeramente 
+
+1/4 Taza Leche Semidescremada LA LECHERA® 
+
+1 Sobre Caldo Criollita MAGGI® 
+
+1 Taza Pescado de preferencia dorado, filete de atún o albacora 
+
+4 Cucharadas Cilantro picado 
+
+1 servicio Limón `,
       recipe: `Prepara el refrito: 
 
 1.Coloca el aceite de achiote junto con la mantequilla en una sartén amplia y, una vez caliente, añade la cebolla paiteña, el tomate, cebolla larga, pimiento y comino. Cocina hasta que todo esté cocido y el refrito formado. Separa la preparación en 2 partes. 
@@ -90,6 +223,29 @@ Cocínalas:
       name: 'Ensalada de fideo con atun',
       ingredientes: 'Fideo tornillo, Atún, Mostaza, Pimienta negra, Cubos de caldo de gallina, Tomate, Apio, Cebolla morada, Pimiento verde, Mostaza, Sal, Pimienta negra, Cilantro, Mayonesa',
       img: 'https://www.recetasnestle.com.ec/sites/default/files/srh_recipes/455efba75ef22885a68be73037486250.png',
+      cantidad_ing: `4 Tazas Fideo Tipo Tornillo 
+
+1 Unidad Cubito de Caldo de Gallina MAGGI® 
+
+2 Tazas Tomate picado finamente 
+
+1 Taza Apio picado finamente 
+
+2 Tazas Atún En Agua escurridos 
+
+1 Taza Cebolla Morada picada en cubitos 
+
+1 Taza Pimiento Verde picado en cubitos 
+
+1 Taza Mayonesa con Limón MAGGI® 
+
+1 Cucharada Mostaza MAGGI® 
+
+1 Cucharadita Sal 
+
+1 Cucharadita Pimienta Negra 
+
+2 Cucharaditas Cilantro picado finamente `,
       recipe: `1.Cocina los fideos junto con 1 Caldo de Gallina en Cubo de 4 gramos MAGGI® hasta que estén suaves. Retíralos del fuego y enfríalos rápidamente en agua fría. Prepara un recipiente transparente y alto. 
 
 2.Coloca en el recipiente, como primera capa, los fideos cocinados. Luego, cubre esta capa con los tomates. A continuación, añade el apio, el atún bien esparcido, la cebolla y el pimiento. 
@@ -100,6 +256,31 @@ Cocínalas:
       name: 'Fritada',
       ingredientes: 'Carne de cerdo, Diente de Ajo, La sazón, Agua, Sobre de caldo de gallina, Aceite vegetal, Mote, Papa chaucha, Platano maduro, Tomate, Cebolla morada, Limón, Cilantro',
       img: 'https://www.aki.com.ec/wp-content/uploads/2021/01/fritada_criolla.jpg',
+      cantidad_ing: `2 Libras Carne De Cerdo cortada en cubos grandes 
+
+2 Unidades Ajo picados finamente 
+
+2 Cucharadas Adobo La Sazón Maggi® 
+
+4 Tazas Agua 
+
+1 Sobre Caldo de Gallina en polvo MAGGI ® 
+
+4 Cucharadas Aceite Vegetal Girasol 
+
+4 Tazas Mote Cocido 
+
+4 Tazas Papas Chauchas lavadas y cocinadas 
+
+1 Unidad Plátano Maduro frito 
+
+1 Unidad Tomate picado en cubos pequeños 
+
+1/2 Unidad Cebolla Morada picada en tiras delgadas 
+
+2 Unidades Limón su jugo 
+
+2 Cucharadas Cilantro picado finamente `,
       recipe: `Prepara la carne 
 
 1.Coloca la carne en un recipiente y sazónala con ajo picado, Adobo LA SAZÓN MAGGI® y medio sobre de Caldo de Gallina en Polvo MAGGI®. Mezcla bien y reserva. 
